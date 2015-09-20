f4d06921f8a1e1a9f9caea22371894a2cbda9eb9
Valerio Crini <vcrini@gmail.com>
2015-09-21 00:10:54 +0200

    writing 'convert' cache to a local directory in order to no broke for insufficient memory while elaborating large cbr files

--- cbr2djvu.py
+++ cbr2djvu.py
@@ -7,6 +7,9 @@ import sys
 from subprocess import call
 import tempfile
 
+env = os.environ.copy()
+env['MAGICK_TMPDIR'] = tempfile.mkdtemp(dir=os.getcwd())
+
 
 def convert(filename):
     directory = tempfile.mkdtemp()
@@ -21,14 +24,25 @@ def convert(filename):
     if len(ll) == 1 and os.path.isdir(inner_path):
         os.chdir(inner_path)
     name = os.path.splitext(filename)[0] + '.djvu'
-    call(['convert', '*', 'foo.pdf'])
+
+    call([
+        'convert',
+        '-limit',
+        'memory',
+        '1',
+        '-limit',
+        'map',
+        '1',
+        '*',
+        'foo.pdf',
+    ], env=env)
     call(['pdf2djvu', 'foo.pdf', '-o', name])
     call(['mv', os.path.join(inner_path, name), old_directory])
-    shutil.rmtree(directory)
 
+    shutil.rmtree(directory)
 
 if __name__ == '__main__':
     old_directory = os.getcwd()
     for fn in sys.argv[1:]:
         convert(fn)
-
+    shutil.rmtree(env['MAGICK_TMPDIR'])
